af5d14a6f5882f46ba4cca6840503e6d020c91dc
Owen Queen <owencqueen@hotmail.com>
2021-11-21 17:29:50 -0500

    clear

--- xtb/md.sh
+++
@@ -1,6 +0,0 @@
-# ./xtb/build/xtb 1-cyclohexylmethyl-4-methoxy-benzene.xyz --input scan.inp --md
-./xtb/build/xtb "base_structs/struct_$1.xyz" --input scan.inp --md
-mv xtb.trj xtb.trj.xyz
-mkdir "../data/xtb_data/base_$1"
-mkdir "../data/xtb_data/base_$1/STRUCTS/"
-python3 split_xyz_files.py 2 xtb.trj.xyz "../data/xtb_data/base_$1"

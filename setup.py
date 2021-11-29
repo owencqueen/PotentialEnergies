f45a9289d4e88b2b325370a53101682f2aad5691
Owen Queen <owencqueen@hotmail.com>
2021-11-29 11:49:35 -0500

    Add all files again

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='chemtda',
+      version='0.1',
+      description='Persistent Homology for Chemistry',
+      packages=['ChemTDA'],
+      zip_safe=False)

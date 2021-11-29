006302ef242d89be1e66e1e56b50d902b07b92d0
Owen Queen <owencqueen@hotmail.com>
2021-11-29 12:30:12 -0500

    delete old data files

--- generate/md.sh
+++ generate/md.sh
@@ -1,5 +1,5 @@
 # ./xtb/build/xtb 1-cyclohexylmethyl-4-methoxy-benzene.xyz --input scan.inp --md
-./xtb/build/xtb "base_structs/struct_$1.xyz" --input scan.inp --md
+../../xtb/xtb/build/xtb "base_structs/struct_$1.xyz" --input scan.inp --md
 mv xtb.trj xtb.trj.xyz
 mkdir "../data/xtb_data/base_$1"
 mkdir "../data/xtb_data/base_$1/STRUCTS/"
